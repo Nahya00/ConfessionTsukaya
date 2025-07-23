45f5a60a3261ebeb3d10b529ee27aadcc728e6b4
Nahya00 <jujulecarambar@gmail.com>
2025-07-23 02:18:08 +0200

    Update confess_bot.py

--- confess_bot.py
+++ confess_bot.py
@@ -5,7 +5,7 @@ import os
 
 TOKEN = os.getenv("DISCORD_TOKEN")  # Remplace par ton token ou définis dans Railway
 GUILD_ID = 1361778893681463436  # Remplace par l'ID de ton serveur
-CONFESS_CHANNEL_ID = 1397368607390044240
+CONFESS_CHANNEL_ID = 1397371944877424651
 LOG_CHANNEL_ID = 1379271452578021459
 
 intents = discord.Intents.default()
